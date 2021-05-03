205d63805dcd68048c34148803632b57b18788de
udden89 <udd.marcus@gmail.com>
2021-05-03 09:42:07 +0200

    Update RenterAccount.java

--- src/main/java/users/RenterAccount.java
+++ src/main/java/users/RenterAccount.java
@@ -1,4 +1,75 @@
 package users;
 
 public class RenterAccount {
+
+    private String id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String profilePicture;
+
+    //<editor-fold desc="----------- GETTER & SETTERS -----------">
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+
+    }
+    //</editor-fold>
+
+
+    @Override
+    public String toString() {
+        return "RenterAccount{" +
+                "id='" + id + '\'' +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", profilePicture='" + profilePicture + '\'' +
+                '}';
+    }
 }
