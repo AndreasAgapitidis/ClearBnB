d229f7a97ef664398c48d9dafbb0146db54fc395
udden89 <udd.marcus@gmail.com>
2021-05-03 12:14:46 +0200

    Update City.java

--- src/main/java/models/city/City.java
+++ src/main/java/models/city/City.java
@@ -10,6 +10,14 @@ public class City {
 
     public City() {    }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
