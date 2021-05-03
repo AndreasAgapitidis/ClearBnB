e5000d99426bbac8c4a34b7244f30ad69f44bbb5
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-05-03 17:04:16 +0200

    Still working on the footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,10 +1,23 @@
 <template>
   <footer>
-  <h1>Hello</h1>
+  <div class="top">
+
+  </div>
+
+  <div class="divider"></div>
+
+  <div class="bottom">
+    <ul>
+      <li><a href="#"><img src='../assets/Logo ClearBnB/Footer/Face.png' alt=""></a></li>
+      <li><a href="#"><img src='../assets/Logo ClearBnB/Footer/Insta.png' alt=""></a></li>
+      <li><a href="#"><img src='../assets/Logo ClearBnB/Footer/Twitter.png' alt=""></a></li>
+    </ul>
+  </div>
   </footer>
 </template>
 
 <script>
+
 export default {
   data(){
     return {
@@ -15,11 +28,43 @@ export default {
 </script>
 
 <style scoped>
-  footer {
 
-  background-color: #454545;
+  footer {
+  background-image: url('../assets/Footer-Header/Footer-Final.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 10em;
+  width: 100vw;
   margin: 0px;
   color: #FAFAFA;
+  display: flex;
+  flex-flow: column;
+}
+
+.top{
+
+}
+
+.divider {
+  background-color: #FAFAFA;
+  height: 1px;
+  width: 90%;
+}
+
+.bottom{
+
+}
+
+.bottom > ul {
+  list-style: none;
+  display: flex;
+  flex-flow: row;
+}
+
+.bottom > li > a > img {
+  height: 50px;
+  widows: 50px;
+  border: 1px solid black;
 }
 
 
