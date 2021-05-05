1e9b529a182b701ee435fbb42a37d27c8dceccd7
udden89 <udd.marcus@gmail.com>
2021-05-05 09:58:31 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -70,4 +70,27 @@ public class Main {
 
   }
 
+    //Temporarily function, used for adding dummy data
+    public static void addNewListing(){
+        Listing newListing = new Listing();
+
+        newListing.setOwner("G3clEb8mGWB0GzrKY5n0Q");
+        newListing.setApartment(true);
+        newListing.setHouse(false);
+        newListing.setCity("Malmö");
+        newListing.setAddress("Bragegatan 31, 241 46");
+        newListing.setDescription("A lovely apartment");
+        newListing.setArea(10);
+        newListing.setBeds(3);
+        newListing.setPrice(650);
+        newListing.setRating(9.5);
+
+        newListing.addBookedDate("id", "date");
+        newListing.addImage("");
+
+        collection(Listing.class).save(newListing);
+
+
+    }
+
 }
