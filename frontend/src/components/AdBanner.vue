809b9eea6b70bd422e48be4fa997f45df40da9b2
AndreasAgapitidis <agapitidisa@gmail.com>
2021-05-06 08:44:07 +0200

    Revert "Revert "Changed all images to href,changed much in  layout and css""
    This reverts commit 8478c048fef368d35f781c3cc8f930c83106d379.

--- frontend/src/components/AdBanner.vue
+++ frontend/src/components/AdBanner.vue
@@ -1,10 +1,38 @@
 <template>
   <div class="component3">
+<<<<<<< Updated upstream
     <img
       class="poolHousePic"
       src="../assets/poolhouse.jpg"
       alt="poolhouse.jpg"
     />
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+    <a href="">
+      <img
+        class="poolHousePic"
+        src="../assets/Houses/HouseWithPool1.jpg"
+        alt="HouseWithPool"
+        width="600"
+        height="400"
+      />
+    </a>
+
+    <button class="bookNowBtn">Book Now!</button>
+=======
+>>>>>>> Stashed changes
+    
+    <div class="picture">
+      <button class="bookNowBtn">Book now!</button>
+    </div>
+    
+<<<<<<< Updated upstream
+=======
+>>>>>>> cbee313fbed084bb82ba3626a2d72bbad7b330fb
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
     <div class="shameLessTxt">
       <label for="shameLessTxt">
@@ -12,8 +40,18 @@
         the italian wine-yards in Toscana.<br /><br /><br />Read more.</label
       >
     </div>
+<<<<<<< HEAD
+=======
 
+<<<<<<< Updated upstream
     <button class="bookNowBtn">Book Now!</button>
+=======
+    
+<<<<<<< Updated upstream
+=======
+>>>>>>> cbee313fbed084bb82ba3626a2d72bbad7b330fb
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
   </div>
 </template>
 
@@ -27,12 +65,37 @@ export default {};
   align-items: flex-end;
   flex-direction: row;
   justify-content: space-between;
+<<<<<<< Updated upstream
   margin-bottom: 10%;
+  width: 100%;
+  height: 300px;
+}
+
+.picture{
+  background-image: url("../assets/poolhouse.jpg");
+  background-size: cover;
+=======
+<<<<<<< HEAD
+   flex-wrap:wrap;
+ 
 }
 
 .poolHousePic {
+=======
+  margin-bottom: 10%;
+}
+
+<<<<<<< Updated upstream
+.poolHousePic {
   width: 50%;
   height: 40%;
+=======
+.picture{
+  background-image: url("../assets/poolhouse.jpg");
+  background-size: cover;
+>>>>>>> cbee313fbed084bb82ba3626a2d72bbad7b330fb
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
   border-radius: 0px 15px 15px 0px;
 }
 
@@ -41,8 +104,28 @@ export default {};
   font-weight: bold;
   padding-inline: 2px 2px;
   color: black;
+<<<<<<< Updated upstream
   margin-bottom: 10%;
   margin-right: 10%;
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+  margin-right: 5%;
+  margin-bottom: 10%;
+=======
+>>>>>>> Stashed changes
+  height: 100%;
+  align-items: center;
+}
+
+span {
+  text-decoration: underline;
+<<<<<<< Updated upstream
+=======
+>>>>>>> cbee313fbed084bb82ba3626a2d72bbad7b330fb
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 }
 
 .bookNowBtn {
@@ -54,8 +137,27 @@ export default {};
   color: white;
   font-weight: bold;
   font-size: 16px;
+<<<<<<< Updated upstream
   cursor: grab;
   margin-bottom: 5%;
   margin-left: 16%;
+=======
+<<<<<<< Updated upstream
+  cursor: pointer;
+  margin-bottom: 20px;
+  outline: none;
+=======
+<<<<<<< HEAD
+  cursor: grab;
+  margin-bottom: 10%;
+  margin-left: 16%;
+  box-shadow: 3px 3px 3px black;
+=======
+  cursor: pointer;
+  margin-bottom: 20px;
+  outline: none;
+>>>>>>> cbee313fbed084bb82ba3626a2d72bbad7b330fb
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 }
 </style>
--- frontend/src/components/LandLordBanner.vue
+++ frontend/src/components/LandLordBanner.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="component6">
-
-   <div class="LookingForCustomers">
+    <div class="LookingForCustomers">
       <label for="LookingForCustomers">Looking For Customers?</label>
     </div>
 
-    <img class="LandLordPic"
+    <img
+      class="LandLordPic"
       src="../assets/landlordpic.png"
       alt="LandLordPic"
-    
     />
-   <button class="LandLordBtn">Become a landlord</button>
-  
+    <button class="LandLordBtn">Become a landlord</button>
   </div>
 </template>
 
@@ -27,34 +25,37 @@ export default {};
   margin-bottom: 10%;
 }
 
-.LandLordPic{
- width:100%;
- height:20%;
+.LandLordPic {
+  width: 100%;
+  height: 20%;
 }
 
-.LandLordBtn{
-  position:absolute;
+.LandLordBtn {
+  position: absolute;
   height: 55px;
   width: 240px;
   border-radius: 20px;
-  background-color: white;
-  color: grey;
+  background-color: black;
+  color: rgb(255, 174, 0);
+  box-shadow: 2px 2px 2px white;
   font-weight: bold;
-  font-size: 16px;
+  font-size: 18px;
   cursor: grab;
   margin-bottom: 5%;
   margin-left: 75%;
-
 }
 
-.LookingForCustomers{
-  position:absolute;
-  font-size: 28px;
-  font-weight: 200px;
+.LookingForCustomers {
+  position: absolute;
+  font-size: 32px;
+  font-weight: bold;
   padding-inline: 2px 2px;
   font-style: italic;
-  color:white;
+  color: rgb(255, 174, 0);
   margin-bottom: 50px;
-  margin-left:5%;
+  margin-left: 5%;
+  padding: 3px 3px;
+  box-shadow: 2px 2px 2px white;
+  text-shadow: 2px 2px rgb(0, 0, 0);
 }
 </style>
