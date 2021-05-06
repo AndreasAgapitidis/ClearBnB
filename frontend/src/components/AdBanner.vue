dfed01e0dc98646b791076072d9326c5819d8bbc
AndreasAgapitidis <agapitidisa@gmail.com>
2021-05-06 08:30:49 +0200

    Revert "Fixed some"
    This reverts commit cbee313fbed084bb82ba3626a2d72bbad7b330fb.

--- frontend/src/components/AdBanner.vue
+++ frontend/src/components/AdBanner.vue
@@ -1,21 +1,19 @@
 <template>
   <div class="component3">
-    
-    <div class="picture">
-      <button class="bookNowBtn">Book now!</button>
-    </div>
-    
+    <img
+      class="poolHousePic"
+      src="../assets/poolhouse.jpg"
+      alt="poolhouse.jpg"
+    />
 
     <div class="shameLessTxt">
       <label for="shameLessTxt">
         Treat yourself and your family.<br />This gem is located close<br />to
-        the italian wine-yards in Toscana.<br /><br /><br />
-        <span>Read more.</span>
-        
-        </label>
+        the italian wine-yards in Toscana.<br /><br /><br />Read more.</label
+      >
     </div>
 
-    
+    <button class="bookNowBtn">Book Now!</button>
   </div>
 </template>
 
@@ -24,59 +22,40 @@ export default {};
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-
 .component3 {
-  font-family: 'Roboto', sans-serif;
   display: flex;
   align-items: flex-end;
   flex-direction: row;
   justify-content: space-between;
   margin-bottom: 10%;
-  width: 100%;
-  height: 300px;
 }
 
-.picture{
-  background-image: url("../assets/poolhouse.jpg");
-  background-size: cover;
+.poolHousePic {
+  width: 50%;
+  height: 40%;
   border-radius: 0px 15px 15px 0px;
-  width: 25em;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
-
 }
-
 
 .shameLessTxt {
-  text-align: right;
-  display: flex;
   font-size: 20px;
   font-weight: bold;
   padding-inline: 2px 2px;
   color: black;
-  height: 100%;
-  align-items: center;
-}
-
-span {
-  text-decoration: underline;
+  margin-bottom: 10%;
+  margin-right: 10%;
 }
 
 .bookNowBtn {
-  font-family: 'Roboto', sans-serif;
+  position: absolute;
   height: 40px;
   width: 200px;
   border-radius: 20px;
-  background-color: #2C2B2B;
+  background-color: rgb(37, 37, 37);
   color: white;
-  font-weight: 100;
+  font-weight: bold;
   font-size: 16px;
-  cursor: pointer;
-  margin-bottom: 20px;
-  outline: none;
+  cursor: grab;
+  margin-bottom: 5%;
+  margin-left: 16%;
 }
 </style>
