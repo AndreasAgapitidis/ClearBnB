4237dc5e4fa7a4ef4c16a7090208a38feef3220d
AndreasAgapitidis <agapitidisa@gmail.com>
2021-05-17 22:08:45 +0200

    testing

---
+++ frontend/src/components/searchComponents/Amenities.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <input
+      type="checkbox"
+      id="WiFi"
+      @click="filterAmenity('Wi-Fi')"
+      value="Wi-Fi"
+    />
+    <label for="Wi-Fi">Wi-Fi</label>
+
+    <input
+      type="checkbox"
+      id="Pool"
+      @click="filterAmenity('Pool')"
+      value="Pool"
+    />
+    <label for="Pool">Pool</label>
+
+    <input
+      type="checkbox"
+      id="Balcony"
+      @click="filterAmenity('Balcony')"
+      value="Balcony"
+    />
+    <label for="Balcony">Balcony</label>
+
+    <input
+      type="checkbox"
+      id="Kitchen"
+      @click="filterAmenity('Kitchen')"
+      value="Kitchen"
+    />
+    <label for="Kitchen">Kitchen</label>
+    <input
+      type="checkbox"
+      id="Freezer"
+      @click="filterAmenity('Freezer')"
+      value="Freezer"
+    />
+    <label for="Freezer">Freezer</label>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    filterAmenity(value) {
+      let arr = [];
+
+      for (let i = 0; i < filteredListings.length; i++) {
+        if (filteredListings.amenities.contains.value("Wi-Fi")) {
+          return this.filteredListings;
+        }
+      }
+
+      this.$parent.$parent.filteredListings = arr;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- frontend/src/components/searchComponents/InputCityAndPickDate.vue
+++ frontend/src/components/searchComponents/InputCityAndPickDate.vue
@@ -24,6 +24,9 @@
       </div>
       <h2>Check-in/out</h2>
       <Calendar />
+
+      <div><h2>Amenities</h2></div>
+      <Amenities />
     </div>
   </div>
 </template>
@@ -31,6 +34,8 @@
 <script>
 import CityItem from "./CityItem.vue";
 import Calendar from "./BookingCalendar.vue";
+import Amenities from "./Amenities.vue";
+
 export default {
   mounted() {
     this.$store.dispatch("fetchCities");
@@ -65,6 +70,7 @@ export default {
   components: {
     CityItem,
     Calendar,
+    Amenities,
   },
   methods: {
     filterIntoUsersChoice(userSearchedFor) {
