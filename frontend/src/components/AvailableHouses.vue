b133955fe435cf683aca29db8304e41364e67020
dogbite1337 <kocak_cc25@hotmail.com>
2021-05-18 14:52:26 +0200

    Updated styling a bit

--- frontend/src/components/AvailableHouses.vue
+++ frontend/src/components/AvailableHouses.vue
@@ -6,28 +6,28 @@
 
     <div v-if="listings[0]" class="HouseCard1">
       <img :src="listings[0].images[0]" />
-      <div class="textHouseWithPool1 text">
+      <div class="priceText text">
         <h4>{{ listings[0].price }}sek</h4>
       </div>
     </div>
 
     <div v-if="listings[1]" class="HouseCard2">
       <img :src="listings[1].images[0]" />
-      <div class="textHouseWithPool2 text">
+      <div class="priceText text">
         <h4>{{ listings[1].price }}sek</h4>
       </div>
     </div>
 
     <div v-if="listings[2]" class="HouseCard3">
       <img :src="listings[2].images[0]" />
-      <div class="textHouse1 text">
+      <div class="priceText text">
         <h4>{{ listings[2].price }}sek</h4>
       </div>
     </div>
 
     <div v-if="listings[3]" class="HouseCard4">
       <img :src="listings[3].images[0]" />
-      <div class="textHouse2 text">
+      <div class="priceText text">
         <h4>{{ listings[3].price }}sek</h4>
       </div>
     </div>
@@ -66,13 +66,13 @@ export default {
 .component4 {
   font-family: "Roboto", sans-serif;
   display: grid;
-  grid-template-columns: auto auto;
+  grid-template-columns: 1fr 1fr;
   grid-template-rows: 10% 0.5fr 0.5fr;
   grid-template-areas:
-    "title ."
+    "title title"
     "H1 H2"
     "H3 H4";
-  grid-gap: 2em 2em;
+  grid-gap: 1em 1em;
   margin-bottom: 15%;
   margin-top: 15%;
   place-content: center;
@@ -81,43 +81,43 @@ export default {
 .availableHouses {
   font-weight: bold;
   grid-area: title;
-  font-size: 15px;
+  font-size: 1.1em;
   width: 100%;
+  text-align: left;
 }
 
 .HouseCard1 {
   grid-area: H1;
   border: 1px solid black;
   border-radius: 20px;
-  width: 150px;
+  min-width: 140px;
+  max-width: 200px;
+  /*width: 150px;*/
   box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
 }
 .HouseCard2 {
   grid-area: H2;
   border: 1px solid black;
   border-radius: 20px;
-  width: 150px;
+  /*width: 150px;*/
   box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
 }
 .HouseCard3 {
   grid-area: H3;
   border: 1px solid black;
   border-radius: 20px;
-  width: 150px;
+  /*width: 150px;*/
   box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
 }
 .HouseCard4 {
   grid-area: H4;
   border: 1px solid black;
   border-radius: 20px;
-  width: 150px;
+  /*width: 150px;*/
   box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
 }
 
-.textHouseWithPool1,
-.textHouseWithPool2,
-.textHouse1,
-.textHouse2 {
+.priceText {
   font-family: "Roboto", sans-serif;
   text-align: center;
   font-size: 20px;
@@ -127,6 +127,7 @@ export default {
 }
 
 .component4 > div > img {
+  object-fit: cover;
   height: 120px;
   width: 150px;
   border-radius: 20px 20px 0 0;
