e14f121be68b5fcb78fc600bfc13d9bd49b7c3f9
dogbite1337 <kocak_cc25@hotmail.com>
2021-05-24 14:06:36 +0200

    Styled the admin summary page

--- frontend/src/components/adminComponents/AdminSummary.vue
+++ frontend/src/components/adminComponents/AdminSummary.vue
@@ -1,26 +1,37 @@
 <template>
   <div class="summary-container">
-    <h1>Summary</h1>
-    <h2>Current Listings</h2>
-    <p>{{ this.totalCurrentListings }}</p>
-    <h2>Reservations (alltime)</h2>
-    <p>{{ this.reservationsAlltime }}</p>
-    <h2>Reservations (this year)</h2>
-    <p>{{ this.reservationsThisYear }}</p>
-    <h2>Reservations (this month)</h2>
-    <p>{{ this.reservationsThisMonth }}</p>
-    <h2>Cashflow (alltime)</h2>
-    <p>{{ this.cashflowAlltime }} sek</p>
-    <h2>Cashflow (this year)</h2>
-    <p>{{ this.cashflowThisYear }} sek</p>
-    <h2>Cashflow (this month)</h2>
-    <p>{{ this.cashflowThisMonth }} sek</p>
-    <h2>Profit (alltime)</h2>
-    <p>{{ this.profitAlltime }} sek</p>
-    <h2>Profit (this year)</h2>
-    <p>{{ this.profitThisYear }} sek</p>
-    <h2>Profit (this month)</h2>
-    <p>{{ this.profitThisMonth }} sek</p>
+    <div class="reservations-container">
+      <h1>Reservations</h1>
+      <h3>This month</h3>
+      <p>{{ reservationsThisMonth }}</p>
+      <h3>This year</h3>
+      <p>{{ reservationsThisYear }}</p>
+      <h3>All time</h3>
+      <p>{{ reservationsAllTime }}</p>
+    </div>
+    <div class="revenue-container">
+      <h1>Revenue</h1>
+      <h3>This month</h3>
+      <p>{{ revenueThisMonth }}</p>
+      <h3>This year</h3>
+      <p>{{ revenueThisYear }}</p>
+      <h3>All time</h3>
+      <p>{{ revenueAllTime }}</p>
+    </div>
+    <div class="profit-container">
+      <h1>Profit</h1>
+      <h3>This month</h3>
+      <p>{{ profitThisMonth }}</p>
+      <h3>This year</h3>
+      <p>{{ profitThisYear }}</p>
+      <h3>All time</h3>
+      <p>{{ profitAllTime }}</p>
+    </div>
+    <div class="listings-container">
+      <h1>Listings</h1>
+      <h3>Current</h3>
+      <p>{{ totalCurrentListings }}</p>
+    </div>
   </div>
 </template>
 
@@ -39,7 +50,7 @@ export default {
     this.totalCurrentListings =  await res.json()
 
     res = await fetch('/api/reservationsAlltime/')
-    this.reservationsAlltime = await res.json()
+    this.reservationsAllTime = await res.json()
 
     res = await fetch('/api/reservationsThisYear/' + currentDate.getFullYear())
     this.reservationsThisYear = await res.json()
@@ -48,31 +59,31 @@ export default {
     this.reservationsThisMonth = await res.json()
 
     res = await fetch('/api/cashflowAlltime/')
-    this.cashflowAlltime = await res.json()
+    this.revenueAllTime = await res.json()
 
     res = await fetch('/api/cashflowThisYear/' + currentDate.getFullYear())
-    this.cashflowThisYear = await res.json()
+    this.revenueThisYear = await res.json()
 
     res = await fetch('/api/cashflowThisMonth/' + currentDate.getFullYear() + '/' + currentMonth)
-    this.cashflowThisMonth = await res.json()
+    this.revenueThisMonth = await res.json()
 
-    this.profitAlltime = Math.round(this.cashflowAlltime * .15)
+    this.profitAllTime = Math.round(this.revenueAllTime * .15)
 
-    this.profitThisYear = Math.round(this.cashflowThisYear * .15)
+    this.profitThisYear = Math.round(this.revenueThisYear * .15)
 
-    this.profitThisMonth = Math.round(this.cashflowThisMonth * .15)
+    this.profitThisMonth = Math.round(this.revenueThisMonth * .15)
   },
 
   data() {
     return {
       totalCurrentListings: null,
-      reservationsAlltime: null,
+      reservationsAllTime: null,
       reservationsThisYear: null,
       reservationsThisMonth: null,
-      cashflowAlltime: null,
-      cashflowThisYear: null,
-      cashflowThisMonth: null,
-      profitAlltime: null,
+      revenueAllTime: null,
+      revenueThisYear: null,
+      revenueThisMonth: null,
+      profitAllTime: null,
       profitThisYear: null,
       profitThisMonth: null,
     }
@@ -80,6 +91,45 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.summary-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto auto;
+  column-gap: 1em;
+  padding: 0 1em 0 1em;
+  margin: auto;
+  max-width: 500px;
+}
+
+.reservations-container {
+  text-align: left;
+}
+
+.revenue-container {
+  text-align: right;
+}
+
+.profit-container {
+  text-align: left;
+}
+
+.listings-container {
+  text-align: right;
+}
+
+h3 {
+  margin-bottom: 8px;
+}
+
+p {
+  border: 1px solid black;
+  border-radius: 20px;
+  padding: 14px;
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 4px;
+  text-align: center;
+}
 
 </style>
