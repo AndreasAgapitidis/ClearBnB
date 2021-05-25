6e2fae7366f9bf61c2b28d3489b9f3b39af7241a
dogbite1337 <kocak_cc25@hotmail.com>
2021-05-25 13:22:52 +0200

    Made user cards responsive on admin page

--- frontend/src/components/userComponents/UserItem.vue
+++ frontend/src/components/userComponents/UserItem.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="userCard">
     <img :src="user.profilePicture" alt="" />
-    <h1>{{ user.firstName }} {{ user.lastName }}</h1>
-    <h1 class="userID">userId: {{ user.id }}</h1>
+    <ul>
+      <li><b>name:</b> {{ user.firstName }} {{ user.lastName }}</li>
+      <li><b>userId:</b> {{ user.id }}</li>
+    </ul>
   </div>
 </template>
 
@@ -19,29 +21,40 @@ export default {
 <style scoped>
 .userCard {
   height: 100px;
+  max-width: 600px;
   background-image: linear-gradient(
     to right,
     rgb(118, 214, 243),
     rgb(253, 253, 253)
   );
-  margin: 10px;
+  margin: 10px auto;
   border-radius: 0 40px 0 40px;
-  display: grid;
-  grid-template-columns: 5vw 50px 25vw 50vw;
-  align-content: center;
-  justify-items: center;
+  display: flex;
+  align-items: center;
 }
-h1 {
-  font-size: 20px;
+
+ul {
+  list-style-type: none;
+}
+
+li {
   text-align: left;
-}
-.userID {
-  grid-column-start: 4;
 }
 
 img {
-  display: flex;
-  grid-column: 2;
-  size: 100%;
+  display: none;
+  margin: 0 20px 0 40px;
+  width: 50px;
+  height: 50px;
 }
+
+@media (min-width: 576px) {
+  li {
+    font-size: 20px;
+  }
+  img {
+    display: block;
+  }
+}
+
 </style>
