f015436804fedbc34d6cb4416041034612e3f959
dogbite1337 <kocak_cc25@hotmail.com>
2021-05-26 14:40:54 +0200

    Minor CSS

--- frontend/src/components/userPageComponents/ProfileBookingList.vue
+++ frontend/src/components/userPageComponents/ProfileBookingList.vue
@@ -23,9 +23,7 @@
         <span>&#8594;</span>
       </p>
 
-      <h5 class="price">
-    
-        {{ reservation.price }}SEK</h5>
+      <h5 class="price">{{ reservation.price }}SEK</h5>
 
       <button v-on:click="select(reservation)" class="cancel">&#10008;</button>
     </div>
@@ -143,6 +141,7 @@ export default {
 .cancel {
   color: red;
   grid-area: ca;
+  cursor: pointer;
 }
 
 button {
